310ecfcd13abcb324d3c2ff52b081279b7a09c25
Jingqing Liu <jingqing@udel.edu>
2024-03-22 16:39:22 -0400

    update default Theme

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -133,7 +133,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'winter',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode

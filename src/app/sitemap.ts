6fc5e9719dc35d52c260616e5e7452570055a234
Jingqing Liu <jingqing@udel.edu>
2026-03-24 01:05:52 -0400

    fix static export: add force-static to robots and sitemap routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next';
 import { researchProjects } from '../data/research';
 import { experienceData } from '../data/experience';
 
+export const dynamic = 'force-static';
+
 const BASE_URL = 'https://jingqing-liu.github.io';
 
 export default function sitemap(): MetadataRoute.Sitemap {
